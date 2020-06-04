28d17dc23fda6cc3c22d328a2fe378c1c423ea93
baitianwei <equweiyu@hotmail.com>
2020-06-04 14:54:41 +0800

    fix for android

--- android/src/main/kotlin/baitianwei/flutter_vod_upload_plugin/FlutterVodUploadPlugin.kt
+++ android/src/main/kotlin/baitianwei/flutter_vod_upload_plugin/FlutterVodUploadPlugin.kt
@@ -1,6 +1,7 @@
 package baitianwei.flutter_vod_upload_plugin
 
 import android.app.Activity
+import android.app.Application
 import androidx.annotation.NonNull
 import com.alibaba.sdk.android.vod.upload.VODUploadCallback
 import com.alibaba.sdk.android.vod.upload.VODUploadClientImpl
@@ -9,60 +10,89 @@ import com.alibaba.sdk.android.vod.upload.model.VodInfo
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
+
 /** FlutterVodUploadPlugin */
 public class FlutterVodUploadPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     var uploader: VODUploadClientImpl? = null
-    var methodChannel: MethodChannel? = null
-    var activity: Activity? = null
+
+
+    private var channel: MethodChannel? = null
+    private var pluginBinding: FlutterPlugin.FlutterPluginBinding? = null
+    private var activityBinding: ActivityPluginBinding? = null
+    private var application: Application? = null
+    private var activity: Activity? = null
+
+    fun setup(messenger: BinaryMessenger, application: Application, activity: Activity) {
+        this.activity = activity
+        this.application = application
+        channel = MethodChannel(messenger, "flutter_vod_upload_plugin")
+        channel!!.setMethodCallHandler(this)
+        uploader = VODUploadClientImpl(application)
+    }
+
+    private fun tearDown() {
+        activityBinding = null
+        channel?.setMethodCallHandler(null)
+        channel = null
+        application = null
+    }
 
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_vod_upload_plugin")
-        val plugin = FlutterVodUploadPlugin()
-        plugin.uploader = VODUploadClientImpl(flutterPluginBinding.applicationContext)
-        channel.setMethodCallHandler(plugin)
-        plugin.methodChannel = channel
+        pluginBinding = flutterPluginBinding
     }
 
-    override fun onDetachedFromActivity() {
-        activity = null
-        methodChannel = null
-        uploader = null
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        this.activity = binding.activity;
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        pluginBinding = null
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        this.activity = binding.activity;
+        this.activityBinding = binding
+        setup(
+                pluginBinding!!.binaryMessenger,
+                (pluginBinding!!.applicationContext as Application),
+                activityBinding!!.activity)
+
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+
+    override fun onDetachedFromActivity() {
+        tearDown()
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
         onDetachedFromActivity()
     }
 
+
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "flutter_vod_upload_plugin")
+            if (registrar.activity() == null) return
+            val activity = registrar.activity()
+            var application: Application? = null
+            if (registrar.context() != null) {
+                application = registrar.context().applicationContext as Application
+            }
             val plugin = FlutterVodUploadPlugin()
-            plugin.uploader = VODUploadClientImpl(registrar.context())
-            channel.setMethodCallHandler(plugin)
-            plugin.methodChannel = channel
-            plugin.activity = registrar.activity()
+            plugin.setup(registrar.messenger(), application!!, activity)
         }
     }
 
     private fun invokeMethod(method: String, arguments: Any?) {
         activity?.runOnUiThread {
-            methodChannel?.invokeMethod(method, arguments)
+            channel?.invokeMethod(method, arguments)
         }
     }
 
@@ -76,11 +106,11 @@ public class FlutterVodUploadPlugin : FlutterPlugin, MethodCallHandler, Activity
                 setUploadAuthAndAddress(call.argument<String>("uploadAuth")!!, call.argument<String>("uploadAddress")!!)
             }
             "addFile" -> {
-                var vodInfo = VodInfo()
+                val vodInfo = VodInfo()
                 val info = call.argument<Map<*, *>>("vodInfo")
                 vodInfo.title = info?.get("title") as? String
                 if (info?.get("isShowWaterMark") is Boolean) {
-                    vodInfo.isShowWaterMark = info?.get("isShowWaterMark") as? Boolean
+                    vodInfo.isShowWaterMark = info["isShowWaterMark"] as? Boolean
                 }
                 addFile(call.argument<String>("filePath")!!, vodInfo)
             }
@@ -88,9 +118,6 @@ public class FlutterVodUploadPlugin : FlutterPlugin, MethodCallHandler, Activity
                 result.notImplemented()
             }
         }
-    }
-
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     }
 
     private fun setUploadAuthAndAddress(uploadAuth: String, uploadAddress: String) {
@@ -125,7 +152,7 @@ public class FlutterVodUploadPlugin : FlutterPlugin, MethodCallHandler, Activity
                 uploader?.setUploadAuthAndAddress(uploadFileInfo, uploadAuth, uploadAddress)
             }
         }
-        uploader?.init(callback);
+        uploader?.init(callback)
     }
 
     private fun addFile(filePath: String, vodInfo: VodInfo) {
